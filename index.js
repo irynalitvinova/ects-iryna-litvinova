c65e5d424f6707e431b61b1b5bc53218237942c9
Iryna Litvinova <lytvynlysko@gmail.com>
2023-11-21 20:11:01 +0200

    Add npm package

---
+++ index.js
@@ -0,0 +1,14 @@
+class ECTS {
+  constructor(score) {
+    this.score = score;
+  }
+  ectsFromScore() {
+    if (this.score >= 90) return "A";
+    if (this.score >= 82) return "B";
+    if (this.score >= 74) return "C";
+    if (this.score >= 65) return "D";
+    if (this.score >= 60) return "E";
+    else return "F";
+  }
+}
+module.exports = ECTS;
